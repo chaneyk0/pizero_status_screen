6d0dd56d52d98b7614b38fb0ae7973587af0985f
Kyle Chaney <kyle@kyle-chaney.com>
2023-12-29 17:15:01 -0800

    added custom status form

--- status.py
+++ status.py
@@ -9,85 +9,111 @@ mylcd = i2c_lcd_driver.lcd()
 app = Flask(__name__)
 #app.config['SERVER_NAME']= 'status.local'
 
+hacking_loop = False
+
+
+
+def switchClear() : 
+    global hacking_loop 
+    mylcd.lcd_clear() 
+    hacking_loop = False 
+    sleep(1)
+
+    
 def currentTime():
     dateraw=datetime.now()
     timeFormat=dateraw.strftime("%-I:%M %p")
     return timeFormat
-   
+
+    
 def switchAvailable() :
     cT=currentTime()
-    mylcd.lcd_clear()
+    switchClear()
     mylcd.lcd_display_string("Available", 1)
     mylcd.lcd_display_string("as of "+cT, 2)
     sleep(1)
-
+    
 def switchBusy() :
     cT=currentTime()
-    mylcd.lcd_clear()
+    switchClear()
     mylcd.lcd_display_string("Busy", 1)
     mylcd.lcd_display_string("as of "+cT, 2)
     sleep(1)
     
 def switchAway() :
     cT=currentTime()
-    mylcd.lcd_clear()
+    switchClear()
     mylcd.lcd_display_string("Away", 1)
-    mylcd.lcd_display_string("as of "+cT, 2)
-    sleep(1)
-    
-def switchSleepy() :
-    cT=currentTime()
-    mylcd.lcd_clear()
-    mylcd.lcd_display_string("Sleepy", 1)
     mylcd.lcd_display_string("as of "+cT, 2)
     sleep(1)
     
 def switchMeeting() :
     cT=currentTime()
-    mylcd.lcd_clear()
+    switchClear()
     mylcd.lcd_display_string("In a meeting", 1)
     mylcd.lcd_display_string("as of "+cT, 2)
     sleep(1)
     
 def switchPhone() :
     cT=currentTime()
-    mylcd.lcd_clear()
+    switchClear()
     mylcd.lcd_display_string("On the phone", 1)
     mylcd.lcd_display_string("as of "+cT, 2)
     sleep(1)
     
 def switchSleepy() :
     cT=currentTime()
-    mylcd.lcd_clear()
+    switchClear()
     mylcd.lcd_display_string("Feeling sleepy", 1)
     mylcd.lcd_display_string("as of "+cT, 2)
     sleep(1)
     
 def switchErrands() :
     cT=currentTime()
-    mylcd.lcd_clear()
+    switchClear()
     mylcd.lcd_display_string("Running errands", 1)
     mylcd.lcd_display_string("as of "+cT, 2)
     sleep(1)    
     
-def switchHacking() :
+def switchFeymood() :
     cT=currentTime()
-    mylcd.lcd_clear()
-    mylcd.lcd_display_string("Hacking the", 1)
-    mylcd.lcd_display_string("Mainframe", 2)
-    sleep(1)   
-    
-def switchClear() :
-    mylcd.lcd_clear()
+    switchClear()
+    mylcd.lcd_display_string("Taken by a fey", 1)
+    mylcd.lcd_display_string("mood as of "+cT, 2)
     sleep(1)
+    
+def switchHacking() :
+    global hacking_loop
+    switchClear()
+    hacking_loop = True
+    mylcd.lcd_display_string("Hacking", 1)
+    
+    while hacking_loop :    
+        for i in range(4):
+            display_string = "Mainframe" + "." * i
+            display_string = display_string.ljust(16)
+            mylcd.lcd_display_string(display_string, 2) 
+            sleep(1)
+
+def display_on_lcd(text):
+    cT=currentTime()
+    switchClear()
+    mylcd.lcd_display_string(text, 1)  
+    mylcd.lcd_display_string("as of "+cT, 2)
     
 # API switchAvailable
 @app.route('/api/available', methods=['GET'])
-def apiavailable() :
+def apiAvailable() :
     switchAvailable()
     return jsonify({})
     
-# API switchHacking
+# API switchFeymood
+@app.route('/api/feymood', methods=['GET'])
+def apiFeymood() :
+    switchFeymood()
+    return jsonify({})
+    
+# API SwitchHacking
 @app.route('/api/hacking', methods=['GET'])
 def apiHacking() :
     switchHacking()
@@ -98,7 +124,7 @@ def apiHacking() :
 def apiBusy() :
     switchBusy()
     return jsonify({})
-
+    
 # API Errands
 @app.route('/api/errands', methods=['GET'])
 def apiErrands() :
@@ -130,13 +156,20 @@ def apiPhone() :
 def apiClear() :
     switchClear()
     return jsonify({})
-
+    
 # API sleepy
 @app.route('/api/sleepy', methods=['GET'])
 def apiSleepy() :
     switchSleepy()
     return jsonify({})
 
+@app.route('/update_custom_status', methods=['POST'])
+def update_custom_status():
+    custom_status = request.form['customStatus']
+    display_on_lcd(custom_status)
+    return 'Status Updated'
+
+    
 @app.errorhandler(404)
 def not_found(error):
     return make_response(jsonify({'error': 'Not found'}), 404)
